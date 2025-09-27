3abef633ac0d0d502cfec4f2abf97ce839a48855
Ayush-dev111 <ayushnegi4d6f@gmail.com>
2025-09-27 23:01:24 +0530

    backend completed

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -141,3 +141,11 @@ export const refreshAccessToken = async  (req, res) => {
         res.status(500).json({message: "Internal server error"});
     }
 };
+
+export const getProfile = async (req, res) => {
+	try {
+		res.json(req.user);
+	} catch (error) {
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
+};
